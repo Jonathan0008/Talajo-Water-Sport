7a62152d180764640f3de20cb2697f35273f87e9
Jonathan0008 <s22110362@student.unklab.ac.id>
2023-11-27 09:50:04 +0700

    first

---
+++ Talajo-Water-Sport/src/App.jsx
@@ -0,0 +1,18 @@
+
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+import Body from "./component/Body"
+
+function App() {
+
+  return (
+    <>
+    <Body/>
+    </>
+  )
+}
+
+export default App
