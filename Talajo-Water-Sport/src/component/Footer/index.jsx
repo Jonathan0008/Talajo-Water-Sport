ae9e2d3be6f48263c140cbec33f0dc2def3a0e44
Jonathan0008 <s22110362@student.unklab.ac.id>
2023-12-03 15:28:54 +0700

    updateing

--- Talajo-Water-Sport/src/component/Footer/index.jsx
+++ Talajo-Water-Sport/src/component/Footer/index.jsx
@@ -8,113 +8,64 @@ const Footer = () => {
             <div className="row">
               <div className="col-lg-3 col-md-6">
                 <div className="footer-info">
-                  <h3>Restaurantly</h3>
+                  <h3>Talajo Water Sports</h3>
                   <p>
-                    A108 Adam Street <br />
-                    NY 535022, USA
+                  JV69+9J9, Minaesa, Talawaan, North Minahasa Regency, North Sulawesi <br />
                     <br />
                     <br />
-                    <strong>Phone:</strong> +1 5589 55488 55
-                    <br />
-                    <strong>Email:</strong> info@example.com
+                    <strong>Phone:</strong> 081143703487
                     <br />
                   </p>
                   <div className="social-links mt-3">
-                    <a href="#" className="twitter">
-                      <i className="bx bxl-twitter" />
+                    <a href="https://www.youtube.com/@phoamelindaagusantoso2178" className="Youtube">
+                      <i className="bx bxl-youtube" />
                     </a>
-                    <a href="#" className="facebook">
+                    <a href="https://www.facebook.com/talajowatersports/" className="facebook">
                       <i className="bx bxl-facebook" />
                     </a>
-                    <a href="#" className="instagram">
+                    <a href="https://www.instagram.com/talajowatersports/" className="instagram">
                       <i className="bx bxl-instagram" />
                     </a>
-                    <a href="#" className="google-plus">
-                      <i className="bx bxl-skype" />
-                    </a>
-                    <a href="#" className="linkedin">
-                      <i className="bx bxl-linkedin" />
+                    <a href="https://www.instagram.com/talajowatersports/" className="instagram">
+                      <i className="bx bxl-instagram" />
                     </a>
                   </div>
                 </div>
               </div>
               <div className="col-lg-2 col-md-6 footer-links">
-                <h4>Useful Links</h4>
+                <h4>Back To Home</h4>
                 <ul>
                   <li>
                     <i className="bx bx-chevron-right" /> <a href="#">Home</a>
                   </li>
-                  <li>
-                    <i className="bx bx-chevron-right" />{" "}
-                    <a href="#">About us</a>
-                  </li>
-                  <li>
-                    <i className="bx bx-chevron-right" />{" "}
-                    <a href="#">Services</a>
-                  </li>
-                  <li>
-                    <i className="bx bx-chevron-right" />{" "}
-                    <a href="#">Terms of service</a>
-                  </li>
-                  <li>
-                    <i className="bx bx-chevron-right" />{" "}
-                    <a href="#">Privacy policy</a>
-                  </li>
                 </ul>
               </div>
               <div className="col-lg-3 col-md-6 footer-links">
-                <h4>Our Services</h4>
+                <h4>Our Developer</h4>
                 <ul>
                   <li>
                     <i className="bx bx-chevron-right" />{" "}
-                    <a href="#">Web Design</a>
+                    <a href="https://www.instagram.com/ger_jr4/">Gerry</a>
                   </li>
                   <li>
                     <i className="bx bx-chevron-right" />{" "}
-                    <a href="#">Web Development</a>
+                    <a href="https://www.instagram.com/saskianjeli/">Anjeli</a>
                   </li>
                   <li>
                     <i className="bx bx-chevron-right" />{" "}
-                    <a href="#">Product Management</a>
+                    <a href="https://www.instagram.com/immanuelaupe/">Imanuela</a>
                   </li>
                   <li>
                     <i className="bx bx-chevron-right" />{" "}
-                    <a href="#">Marketing</a>
+                    <a href="https://www.instagram.com/mndacm/">Amanda</a>
                   </li>
                   <li>
                     <i className="bx bx-chevron-right" />{" "}
-                    <a href="#">Graphic Design</a>
+                    <a href="https://www.instagram.com/eeeeren_/">Lovencia</a>
                   </li>
                 </ul>
               </div>
-              <div className="col-lg-4 col-md-6 footer-newsletter">
-                <h4>Our Newsletter</h4>
-                <p>
-                  Tamen quem nulla quae legam multos aute sint culpa legam
-                  noster magna
-                </p>
-                <form action method="post">
-                  <input type="email" name="email" />
-                  <input type="submit" defaultValue="Subscribe" />
-                </form>
-              </div>
             </div>
-          </div>
-        </div>
-        <div className="container">
-          <div className="copyright">
-            © Copyright{" "}
-            <strong>
-              <span>Restaurantly</span>
-            </strong>
-            . All Rights Reserved
-          </div>
-          <div className="credits">
-            {/* All the links in the footer should remain intact. */}
-            {/* You can delete the links only if you purchased the pro version. */}
-            {/* Licensing information: https://bootstrapmade.com/license/ */}
-            {/* Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/restaurantly-restaurant-template/ */}
-            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
           </div>
         </div>
       </footer>
